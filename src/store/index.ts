b9e4cb00d038cf7fd995662b9e750fb24202e276
Daria <airadavometra@yandex.ru>
2021-06-09 17:19:17 +0300

    style checkbox

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { fooReducer } from '@store/slices/foo';
 import { exampleReducer } from '@store/slices/example';
 
 const reducer = {
-  foo: fooReducer,
   example: exampleReducer,
 };
 
